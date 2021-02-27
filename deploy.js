af7a22deda0d8670bd855fd67cfbc06e240aed66
Hans Gehman <thehansgehman@gmail.com>
2021-02-26 18:16:17 -0600

    updated ip address"

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.86.242.159',
+      host: '34.239.101.90',
       username: 'ubuntu',
       privateKey: 'finalproject-key.pem'
     })
